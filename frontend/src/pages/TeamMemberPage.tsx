bf1f02f3a3fc29769662601dec8b693bdc645e21
Sarthak Chaturvedi <sarthakchat2018@gmail.com>
2026-02-01 01:31:23 +0530

    TeamMembers Page Done.

--- frontend/src/pages/TeamMemberPage.tsx
+++ frontend/src/pages/TeamMemberPage.tsx
@@ -1,5 +1,8 @@
 import { useOutletContext } from "react-router";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { getTeamMember } from "../api/teamMember.api";
+
 
 type DashboardContext = {
     team : any
@@ -7,10 +10,24 @@ type DashboardContext = {
 
 export default function TeamMemberPage(){
     const {team} = useOutletContext<DashboardContext>();
+    const [teamMembers, setTeamMembers] = useState<any>([]);
+    useEffect(() => {
+        if(!team._id) return;
+        getTeamMember(team._id).then(setTeamMembers);
+    }, [team._id]);
+    if(!team) return <div>No Team</div>
     return(
         <div>
             <Navbar teamName={team.name} heading="Team Members" subLine="All your team members"/>
-            Welcome to Team Member Page
+            <div className="m-10 flex justify-evenly">
+                {
+                    teamMembers.map((tm : any) => (
+                       <div key={tm._id} className="shadow-lg bg-gray-200 w-50 h-20 flex justify-center items-center">
+                            <h3>{tm.userId.name}</h3>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
